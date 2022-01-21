0b574d4a0bc30bd573ac5d8a2bf3bb2a2708723d
Felipe Araujo <felipe.trovadearaujo@digidentity.com>
2022-01-21 18:06:35 +0100

    RabbitMQ

--- notifications/src/main/java/com/fetrova/notifications/NotificationsApplication.java
+++ notifications/src/main/java/com/fetrova/notifications/NotificationsApplication.java
@@ -1,15 +1,34 @@
 package com.fetrova.notifications;
 
+import com.fetrova.amqp.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @EnableEurekaClient
-@SpringBootApplication
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.fetrova.notifications",
+                "com.fetrova.amqp"
+        }
+)
 public class NotificationsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NotificationsApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer, NotificationConfig notificationConfig) {
+        return args -> {
+            producer.publish(
+                    "foo",
+                    notificationConfig.getInternalExchange(),
+                    notificationConfig.getInternalNotificationRoutingKey()
+            );
+        };
     }
 
 }
